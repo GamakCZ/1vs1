e6ff56771bfae8d37470ae6a4387fe4e7ffc29ef
Nguyễn Đạt <89953941+BeeAZZ@users.noreply.github.com>
2022-03-15 20:35:05 +0700

    Update ArenaScheduler.php

--- 1vs1/src/vixikhd/onevsone/arena/ArenaScheduler.php
+++ 1vs1/src/vixikhd/onevsone/arena/ArenaScheduler.php
@@ -70,7 +70,7 @@ class ArenaScheduler extends Task {
         switch ($this->plugin->phase) {
             case Arena::PHASE_LOBBY:
                 if(count($this->plugin->players) >= 2) {
-                    $this->plugin->broadcastMessage("§a> Starting in " . Time::calculateTime($this->startTime) . " sec.", Arena::MSG_TIP);
+                    $this->plugin->broadcastMessage("§a> Starting in " . Time::calculateTime($this->startTime) . " sec.", Arena::MSG_POPUP);
                     $this->startTime--;
                     if($this->startTime == 0) {
                         $this->plugin->startGame();
@@ -102,7 +102,7 @@ class ArenaScheduler extends Task {
                     case 0:
 
                         foreach ($this->plugin->players as $player) {
-                            $player->teleport($this->plugin->plugin->getServer()->getWorldManager()->getDefaultWorld()->getSafeSpawn());
+                            $player->teleport($this->plugin->getServer()->getWorldManager()->getDefaultWorld()->getSafeSpawn());
 
                             $player->getInventory()->clearAll();
                             $player->getArmorInventory()->clearAll();
@@ -111,7 +111,7 @@ class ArenaScheduler extends Task {
                             $player->getHungerManager()->setFood(20);
                             $player->setHealth(20);
 
-                            $player->setGamemode($this->plugin->plugin->getServer()->getGamemode());
+                            $player->setGamemode($this->plugin->getServer()->getGamemode());
                         }
                         $this->plugin->loadArena(true);
                         $this->reloadTimer();
@@ -124,7 +124,7 @@ class ArenaScheduler extends Task {
     public function reloadSign() {
         if(!is_array($this->plugin->data["joinsign"]) || empty($this->plugin->data["joinsign"])) return;
 
-        $signPos = Position::fromObject(Vector3::fromString($this->plugin->data["joinsign"][0]), $this->plugin->plugin->getServer()->getWorldManager()->getWorldByName($this->plugin->data["joinsign"][1]));
+        $signPos = Position::fromObject(Vector3::fromString($this->plugin->data["joinsign"][0]), $this->plugin->getServer()->getWorldManager()->getWorldByName($this->plugin->data["joinsign"][1]));
 
         if(!$signPos->getWorld() instanceof World) return;
 
