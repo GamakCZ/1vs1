ca667e1112e8dc54e6e83fdc21abf38e563053c4
Nguyễn Đạt <89953941+BeeAZ-pm-pl@users.noreply.github.com>
2022-04-29 18:42:18 +0700

    Update Arena.php

--- 1vs1/src/vixikhd/onevsone/arena/Arena.php
+++ 1vs1/src/vixikhd/onevsone/arena/Arena.php
@@ -45,7 +45,7 @@ use vixikhd\onevsone\event\PlayerEquipEvent;
 use vixikhd\onevsone\math\Vector3;
 use vixikhd\onevsone\OneVsOne;
 use pocketmine\Server;
-use vixikhd\onevsone\arena\ArenaSchedule;
+use vixikhd\onevsone\arena\ArenaScheduler;
 
 /**
  * Class Arena
